2f26c1bfe97ab1f2afbe61ac8611d0219ad7caec
Yahya002CY <yahya.kabbassi02@gmail.com>
2025-12-17 14:34:43 +0100

    QUESTION 4 ET 5

--- main.c
+++ main.c
@@ -2,24 +2,32 @@
 #include <string.h>
 #include <sys/wait.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
 
 #define MAX_SIZE 128
 #define WELCOME_MSG "Bienvenue dans le Shell ENSEA.\nPour quitter, tapez 'exit'.\n"
-#define PROMPT "enseash % "
 #define BYE_MSG "Bye bye...\n"
+
+
+void print_msg(const char *msg){
+     write(STDOUT_FILENO,msg ,strlen(msg));
+}
 
 int main() {
     char command[MAX_SIZE];
+    char prompt[MAX_SIZE]="enseash%";
     int status;
     ssize_t ret;
+    struct timespec start,end;
     pid_t pid;
+    
 
-    // Display welcome message
-    write(STDOUT_FILENO, WELCOME_MSG, strlen(WELCOME_MSG));
+    print_msg(WELCOME_MSG);
 
     while (1) {
         // Display prompt
-        write(STDOUT_FILENO, PROMPT, strlen(PROMPT));
+        print_msg(prompt);
 
         // Read user input
         ret = read(STDIN_FILENO, command, MAX_SIZE);
@@ -29,7 +37,7 @@ int main() {
         // 1. Handle <ctrl>+d
         // If read returns 0, it means End Of File (EOF)
         if (ret == 0) {
-            write(STDOUT_FILENO, BYE_MSG, strlen(BYE_MSG));
+           print_msg(BYE_MSG);
             break; // Exit the loop
         }
 
@@ -42,7 +50,8 @@ int main() {
             write(STDOUT_FILENO, BYE_MSG, strlen(BYE_MSG));
             break; // Exit the loop
         }
-       
+        
+	clock_gettime(CLOCK_MONOTONIC,&start);
         // ------------------------------
 
         // Execution (Standard fork/exec/wait)
@@ -56,6 +65,18 @@ int main() {
         } else {
             wait(&status);
         }
+
+	clock_gettime(CLOCK_MONOTONIC,&end);
+	long elapsed_ms=(end.tv_sec-start.tv_sec)*1000 +(end.tv_nsec - start.tv_nsec)/1000000 ;
+         
+        //WIFEXITED checks for normal termination.
+	//WIFSIGNALED checks if the process was killed by a signal
+ 	if(WIFEXITED(status)){
+ 	snprintf(prompt,sizeof(prompt),"enseash [exit: %d|%ldms]%%",WEXITSTATUS(status),elapsed_ms);
+     }  else if (WIFSIGNALED(status)){
+ 	snprintf(prompt,sizeof(prompt),"enseash[sign:%d|%ldms]%%",WTERMSIG(status),elapsed_ms);
+     }      
+     
     }
     return 0;
 }
